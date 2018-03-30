1cc1bd64b9aa733e701e296f7a659e135c350d28
Brian Davey <bdavey2@nd.edu>
2018-03-30 13:26:20 -0400

    renamed file

--- stock_data/meanStdv.py
+++ stock_data/meanStdv.py
@@ -1,74 +1,58 @@
 
 # coding: utf-8
 
-# In[5]:
-
+###
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
+#get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[6]:
-
+###
 
 ticker_df = pd.read_csv('ticker_data.csv')
 factor_df = pd.read_csv('factor_data.csv')
 
 
-# In[7]:
-
-
-print(factor_df.head())
-
-
-# In[36]:
-
+###
 
 ticker_df = ticker_df.sort_values(by=['ticker','timestep'])
 
-#tick_dict = {}
-#keys = np.arange(0,1001,1)
 
 
 mean = []
 stdev = []
+sharpe = []
 
 return_array = np.zeros((2519,1000))
 z = 0
 
 for i in range(1,2520000,2520):
     return_array[:,z] = ticker_df['returns'][i:i+2519]
-    z += 1
     mean.append(ticker_df['returns'][i:i+2519].mean())
     stdev.append(ticker_df['returns'][i:i+2519].std())
-    
-#print(mean)
-#print(stdev)
+    sharpe.append(mean[z]/stdev[z])
+    z += 1
+
 
 plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-#plt.plot(ticker_df['timestep'][:2520],ticker_df['returns'][:2520])
 
-#print(ticker_df.head())
 
 plt.plot(stdev,mean,'o')
 plt.xlabel('Volatility')
 plt.ylabel('Expected Return')
 
-print(return_array)
 
 
-# In[ ]:
-
+###
 
 def rand_weights(n):
     k = np.random.rand(n)
     return k/sum(k)
 
 
-# In[56]:
-
+###
 
 def random_portfolio(returns):
     '''Returns the mean and standard deviation of returns'''
@@ -83,35 +67,8 @@ def random_portfolio(returns):
     return mu, sigma
 
 
-# In[57]:
 
-
-return_vec = np.random.randn(4,100)
-print(return_vec)
-
-
-# In[58]:
-
-
-#array1 = np.array([[1,2],[3,4]])
-
-#print(array1[1,0])
-
-# row then column
-
-
-# In[53]:
-
-
-#array2 = np.zeros((2,4))
-
-
-#array2[:,1] = [1,2]
-#print(array2)
-
-
-# In[54]:
-
+###
 
 n_portfolios = 500
 means, stds = np.column_stack([
@@ -120,8 +77,7 @@ means, stds = np.column_stack([
 ])
 
 
-# In[55]:
-
+###
 
 plt.plot(stds, means, 'o', markersize=5)
 plt.xlabel('std')
