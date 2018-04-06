161c0778e78583cd9ed365e2581822bd8ba55772
James Francis Farrington <jfarrin1@dsg1.crc.nd.edu>
2018-04-06 13:36:09 -0400

    pandas is AIDS

--- neural_network.py
+++ neural_network.py
@@ -1,36 +1,59 @@
 from sklearn.neural_network import MLPRegressor
 import numpy as np
-import matplotlib
-matplotlib.use('agg')
+import pandas as pd
 import matplotlib.pyplot as plt
-#import matplotlib.pyplot as plt
 
  
-'''ticker_df = pd.read_csv('stock_data/ticker_data.csv')
+ticker_df = pd.read_csv('stock_data/ticker_data.csv')
 factor_df = pd.read_csv('stock_data/factor_data.csv')
+
+ticker_df = ticker_df.iloc[1000:]
+ticker_df.sort_values(['ticker','timestep'], inplace=True)
+factor_df = factor_df.iloc[1:]
+
 ticker_df.set_index('timestep', inplace=True)
 factor_df.set_index('timestep', inplace=True)
 stock_df = ticker_df.join(factor_df, how='left')
 
-returns = stock_df[['returns']]
-stock_df.drop('returns')
+stock_df.sort_values(['ticker', 'index'], inplace=True)
 stock_df = stock_df.as_matrix()
-'''
+print(stock_df[0:10][4])
+print("============================")
 
-ticker = np.genfromtxt('stock_data/ticker_data_test.csv', delimiter=",")
-features = np.genfromtxt('stock_data/factor_data.csv', delimiter=",")
+# -- I want ticker to be just the returns for all tickers timestamp 1 and on (ordered by ticker then timestamp -- 
+ticker = stock_df[1][:][4]
 
-features_train = features[2:1512,:9]
-ticker_train = ticker[2:1512,4]
-features_test = features[1512:,:9]
-ticker_test = ticker[1512:, 4]
-nn = MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam')
+# -- I want features to be the factors_df for timestamp 1 and on, but repeated 1000 times, 1 for each stock (so timetamp 1-2520 then again 1-2520 a 1000 times)
+features = stock_df[:][:]
+
+#ticker = np.genfromtxt('stock_data/ticker_data.csv', delimiter=",")
+#features_raw = np.genfromtxt('stock_data/factor_data.csv', delimiter=",")
+#ticker = ticker_df.as_matrix()
+#ticker = ticker[ticker[:,5].argsort()]
+print(ticker)
+print('=========')
+print(features[:10][:])
+#features = np.empty(0, dtype=float)
+#for ix in range(1000):
+#	features = np.append(features,features_raw[2:][:])
+#	for i in range(999):
+#		features.insert(features[i][:],i)
+print(len(features))
+print(len(ticker))
+features_train = features[0:1512000,:10]
+ticker_train = ticker[0:1512000,4]
+features_test = features[1512000:2520000,:10]
+ticker_test = ticker[1512000:2520000, 4]
+nn = MLPRegressor(hidden_layer_sizes=(100,),  activation='relu', solver='adam', alpha=0.001, batch_size='auto', learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=1000, shuffle=True, random_state=0, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08) 
 
 n = nn.fit(features_train, ticker_train)
 
 predicted_returns = nn.predict(features_test)
 
+output = np.subtract(predicted_returns,ticker_test)
+output = np.absolute(output)
 
+print(output.mean())
 
 
 
